8a28ba0f7d64da0caeb39d55d9e4e5dd04e1c1f3
Pablo Gamba <pablogamba@outlook.com>
2021-01-11 16:18:57 +0100

    UnitTest UI - RecyclerView

---
+++ app/src/androidTest/java/com/example/punkBeerSample/ui/beersList/BeersListFragmentTest.kt
@@ -0,0 +1,54 @@
+package com.example.punkBeerSample.ui.beersList
+
+import androidx.test.espresso.Espresso.onView
+import androidx.test.espresso.action.ViewActions.click
+import androidx.test.espresso.assertion.ViewAssertions.matches
+import androidx.test.espresso.matcher.ViewMatchers.*
+import androidx.test.ext.junit.rules.ActivityScenarioRule
+import androidx.test.ext.junit.runners.AndroidJUnit4
+import androidx.test.filters.LargeTest
+import com.example.punkBeerSample.MainActivity
+import com.example.punkBeerSample.R
+import org.hamcrest.BaseMatcher
+import org.hamcrest.CoreMatchers.allOf
+import org.hamcrest.Description
+import org.hamcrest.Matcher
+import org.junit.Rule
+import org.junit.Test
+import org.junit.runner.RunWith
+
+@RunWith(AndroidJUnit4::class)
+@LargeTest
+class BeersListFragmentTest {
+
+    @get:Rule
+    val activityRule = ActivityScenarioRule(MainActivity::class.java)
+
+    @Test
+    fun listBeerVisible() {
+        onView(withId(R.id.recyclerviewlist)).check(matches(isDisplayed()))
+    }
+
+    @Test
+    fun clickOnListBeerItem() {
+        onView(allOf(isDisplayed(), first(withParent(withId(R.id.recyclerviewlist)))))
+            .perform(click())
+    }
+
+    private fun <T> first(matcher: Matcher<T>): Matcher<T>? {
+        return object : BaseMatcher<T>() {
+            var isFirst = true
+            override fun matches(item: Any): Boolean {
+                if (isFirst && matcher.matches(item)) {
+                    isFirst = false
+                    return true
+                }
+                return false
+            }
+
+            override fun describeTo(description: Description) {
+                description.appendText("should return first matching item")
+            }
+        }
+    }
+}
